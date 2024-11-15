3d145634587fe7e113a26cdf0b90b459f603c4ef
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-11-15 07:46:51 -0300

    Finalizando projeto

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def main():
+    first_number = float(input("Enter the first number: "))
+    second_number = float(input("Enter the second number: "))
+    result = first_number + second_number
+    print(f"The sum of the two numbers is: {result}")
+
+
+if __name__ == "__main__":
+    main()
